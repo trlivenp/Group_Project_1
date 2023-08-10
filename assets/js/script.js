471729d959411d47844e2e8098a1a8ea06fe228c
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-08-09 20:49:32 -0400

    Swtiched Search bar with dropdown menu. Reorganized page slightly. Added basic movie list (not using API) for use in JavaScript building

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,36 @@ try {
 } catch (error) {
 	console.error(error);
 }
+
+function createCard(imageSrc, tag, title) {
+  return `
+    <div class="card u-clearfix">
+      <div class="card-media">
+        <img src="${imageSrc}" alt="" class="card-media-img" />
+        <div class="card-media-preview u-flex-center">
+          <svg fill="#ffffff" height="18" viewBox="0 0 24 24" width="18" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8 5v14l11-7z"/>
+            <path d="M0 0h24v24H0z" fill="none"/>
+          </svg>
+        </div>
+        <span class="card-media-tag card-media-tag-${tag.toLowerCase()}">${tag}</span>
+      </div>
+      <div class="card-body">
+        <h2 class="card-body-heading">${title}</h2>
+        <!-- ... Other card body elements ... -->
+      </div>
+    </div>
+  `;
+}
+
+const cardsData = [
+  { imageSrc: "https://s18.postimg.cc/v0mympf7t/lmf1.jpg", tag: "Action", title: "Batman" },
+  { imageSrc: "https://s12.postimg.cc/t0h9q7999/lmf0.jpg", tag: "Western", title: "Lone Ranger" },
+  { imageSrc: "https://s13.postimg.cc/h8spyr37b/lmf2.jpg", tag: "Action", title: "Superman" },
+  // ... Add more cards data here ...
+];
+
+const container = document.querySelector(".container");
+cardsData.forEach(data => {
+  container.innerHTML += createCard(data.imageSrc, data.tag, data.title);
+});
