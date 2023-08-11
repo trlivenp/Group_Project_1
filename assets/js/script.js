b8be55b634a1cf4d702ccc6e2882b4cc86017724
jeslocascio <134552933+jeslocascio@users.noreply.github.com>
2023-08-10 20:36:11 -0400

    Sharing What I have right now for Modal code

--- assets/js/script.js
+++ assets/js/script.js
@@ -242,3 +242,29 @@ fetch(serviceUrl, options)
 } catch (error) { //If something goes wrong, then do this to alert the user
 console.error(error);
 }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
