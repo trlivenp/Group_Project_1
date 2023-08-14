7bf95fe3a364adab1734fea5ea1be7340cf78679
Jonathan Maldonado Guemarez <cantor.dedekind@gmail.com>
2023-08-14 16:18:05 -0400

    The modals now open: we fetch data from Streaming Availability in order to display it on the modal. The modal includes links to the service's websites and imdb, all related to the chosen title.

--- script4.js
+++ script4.js
@@ -1,35 +1,14 @@
-/*In this experiment I am trying to filter the results by genre (initially choosing "horror" id=80) so that only movies that are in english and with a score of at least 8 are shown. There are issues with pagination,
-so I decided to iterate through the pages by employing a loop*/
-//Declaring the function that will be called in order to display a card for each movie title
-function createCard(imageSrc, tag, title, summary, rating, runtime) {
-    return `
-      <div class="card u-clearfix">
-        <div class="card-media">
-          <img src="${imageSrc}" alt="${title}-poster" class="card-media-img" />
-          <div class="card-media-preview u-flex-center">
-            <svg fill="#ffffff" height="18" viewBox="0 0 24 24" width="18" xmlns="http://www.w3.org/2000/svg">
-              <path d="M8 5v14l11-7z"/>
-              <path d="M0 0h24v24H0z" fill="none"/>
-            </svg>
-          </div>
-          <span class="card-media-tag card-media-tag-${tag}">${tag}</span>
-        </div>
-        <div class="card-body">
-          <button type="btn" class="get-modal card-body-heading" >${title}</button>
-          <span><strong> ${rating}</strong> </span>
-          <span> ${runtime} </span>
-          <p id="summary" class="text-sm font-normal text-gray-500 dark:text-gray-400">${summary}</p> 
-      </div>
-    `;
-  }
-  const container = document.querySelector(".container");
- 
 
+const container = document.querySelector(".container");
+ 
 var genreList = document.getElementById("genre-list");
 
 var selectedGenre; 
 
 var advMovieSearchEndpoint = 'https://advanced-movie-search.p.rapidapi.com/discover/movie/?with_genres=';
+
+/*By using event-delegation, when a genre from the dropdown menu is selected, a fetch request will be sent to Advanced Movie Search API 
+in order to  find titles of high-rated (score == 7 or higher) english language movies that belong to the chosen genre*/
 
 genreList.addEventListener("click", function(event){
   
@@ -37,60 +16,61 @@ genreList.addEventListener("click", function(event){
 
   var element = event.target;
 
-var genreListItem = element.parentElement;
-selectedGenre = genreListItem.className;
+  var genreListItem = element.parentElement;
+  selectedGenre = genreListItem.className;
 
 
-searchGenre= advMovieSearchEndpoint + selectedGenre;
+  searchGenre= advMovieSearchEndpoint + selectedGenre;
 
-const advMovieSearchOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'aed62a6282msh245eac67e8ef1f5p1a61d3jsn48e680928113',
-		'X-RapidAPI-Host': 'advanced-movie-search.p.rapidapi.com'
-	}
-};
-
-var searchURL ;
-
-for(let k=0; k<10; k++){ //Iterating through the first 10 pages in order to find "horror" movies in english and with a vote average of at least 8.
-
-   searchURL = searchGenre +'&page='+ k;
-  
-  fetch(searchURL, advMovieSearchOptions)
-  .then(function (response) {
-    if (!response.ok) { //If the response status is not within the 200s range, then halt the execution of the fetch request
-      return;
+  const advMovieSearchOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'c474a02743mshb4a9aeef3843b0dp1c5eeajsn7330f2e8aae3',
+      'X-RapidAPI-Host': 'advanced-movie-search.p.rapidapi.com'
     }
-  
-    var result = response.json();
-    console.log(result);
-  
-    return result;
-  }).then(function (data){
-   
-    console.log(data); //An object with the following properties: page, results, total_pages, total_results
-    console.log(data.results); //An array with 20 elements, each one of them representing a movie (about 20 movies per page)
-    console.log(data.results.length);//Outputs "20"
-  
-    for(i=0; i<data.results.length; i++){
-  
-      if(data.results[i].vote_average >= 7 && data.results[i].original_language =="en"){
-      console.log(data.results[i].original_title);
+  };
 
-      //data.results[i].original_title; //The variable temporarily stores the title of a movie from the chosen genre.
+  var searchURL ;
 
-      console.log(data.results[i].id); //Apparently, it is not the imdb.com id
-      
-      fetchOmdbInfo(data.results[i].original_title);//For each movie from the selected genre, with a score of at least 8 and originally in english, we are going to gather data using OMDB in order to create a card for it.
-      
+  for(let k=0; k<10; k++){ //Iterating through the first 5 pages in order to find "horror" movies in english and with a vote average of at least 8.
+
+    searchURL = searchGenre +'&page='+ k;
+    
+    fetch(searchURL, advMovieSearchOptions)
+    .then(function (response) {
+      if (!response.ok) { //If the response status is not within the 200s range, then halt the execution of the fetch request
+
+        console.log(response.statusText);
+
+        return;
 
       }
-    }
-  });
-  }
+     
+      console.log(response.json());
+    
+    }).then(function (advMovieData){
+    
+      console.log(advMovieData); //An object with the following properties: page, results, total_pages, total_results
+      console.log(advMovieData.results); //An array with 20 elements, each one of them representing a movie (about 20 movies per page)
+      //console.log(data.results.length);//Outputs "20"
+    
+      for(let i=0; i<advMovieData.results.length; i++){
+    
+        if(advMovieData.results[i].vote_average >= 7 && advMovieData[i].original_language =="en"){
+        //console.log(data.results[i].original_title);
 
-})
+        //data.results[i].original_title; //The variable temporarily stores the title of a movie from the chosen genre.
+
+        //console.log(data.results[i].id); //Apparently, it is not the imdb.com id
+        
+        fetchOmdbInfo(advMovieData.results[i].original_title);//For each movie from the selected genre, with a score of at least 8 and originally in english, we are going to gather data using OMDB in order to create a card for it.
+        
+
+        }
+      }
+    });
+    }
+  })
 
  //Function to handle OMDB fetch request by movie title.
 
@@ -107,22 +87,23 @@ for(let k=0; k<10; k++){ //Iterating through the first 10 pages in order to find
   fetch(omdbURL)
    .then(function (response) {
      if (!response.ok) { //If the response status is not within the 200s range, then halt the execution of the fetch request
-       return;
+       
+      console.log(response.statusText)
+
+      return;
      }
-     var result= response.json();
-     console.log(result);
-     return result;
-     }).then(function (omdbData){
+
+     console.log(response.json());
+
+     return;
+     })
+     .then(function (omdbData){
  
      console.log(omdbData);
-     console.log(omdbData.Plot);
-     //oData.Plot;
-     console.log(omdbData.Poster)
-
-     //var moviePosterUrl = omdbData.Poster;
    
-     
      container.innerHTML += createCard(omdbData.Poster, omdbData.Genre, movieTitle, omdbData.Plot, omdbData.Rated, omdbData.Runtime); //Calling the createCard function in order to create another movie card and add it to the <div> with class "".container"
+     /*All of the data below could be retrieved if we wanted to*/
+     
      //console.log(oData.Actors);
      //console.log(oData.Country);
      //console.log(oData.Director);
@@ -141,37 +122,59 @@ for(let k=0; k<10; k++){ //Iterating through the first 10 pages in order to find
    });
  
  }
-   var selectedTitle; //The user will select the title by clicking on the corresponding movie card.
-   //This arrays will be used to temporarily store the data fetched from Streaming Availability, especially while looping through the raw data.
-   var movieStreamOpts = [];
-   var movieStreamOpt = new Array (5);
-   var movieData = new Array(4);
-   
-   
-   // Get the modal
-   var modal = document.getElementById("myModal");
-   
-   // Get the button that opens the modal for each one of the movie cards. The text content of the button is the movie's title.
-   var btnCollection = document.getElementsByClassName("get-modal");
-   
-   console.log(btnCollection);
-   
-   // Get the <span> element that closes the modal
-   var span = document.getElementsByClassName("close")[0];
-   
-   /* When the user clicks on the button, open the modal, "Hello" is displayed to the console(for testing purposes) and the title is selected*/
+
+ //Declaring the function that will be called by the fetchOmdbInfo function in order to create and display a card for each movie title from the chosen genre
+
+function createCard(imageSrc, tag, title, summary, rating, runtime) {
+  return `
+    <div class="card u-clearfix">
+      <div class="card-media">
+        <img src="${imageSrc}" alt="${title}-poster" class="card-media-img" />
+        <div class="card-media-preview u-flex-center">
+          <svg fill="#ffffff" height="18" viewBox="0 0 24 24" width="18" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8 5v14l11-7z"/>
+            <path d="M0 0h24v24H0z" fill="none"/>
+          </svg>
+        </div>
+        <span class="card-media-tag card-media-tag-${tag}">${tag}</span>
+      </div>
+      <div class="card-body">
+        <button type="btn" class="get-modal card-body-heading" onclick = "getModal(event)" >${title}</button>
+        <span><strong> ${rating}</strong> </span>
+        <span> ${runtime} </span>
+        <p id="summary" class="text-sm font-normal text-gray-500 dark:text-gray-400">${summary}</p> 
+    </div>
+  `;
+}
+var selectedTitle; //The user will select the title by clicking on the corresponding movie card.
+//This arrays will be used to temporarily store the data fetched from Streaming Availability, especially while looping through the raw data.
+var movieStreamOpts = [];
+var movieStreamOpt = new Array (5);
+var movieData = new Array(4);
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal for each one of the movie cards. The text content of the button is the movie's title.
+var btnCollection = document.getElementsByClassName("get-modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
   
-   for(i=0; i<btnCollection.length; i++){
-    btnCollection[i].onclick = function(event) {
-      modal.style.display ="block";
-      console.log("Hello");
-      selectedTitle = event.target.innerHTML;
-      console.log(selectedTitle);
-      fetchStreamingAvailability(selectedTitle, movieData);
-    }
-  }
-   
-   
+var streamOptsList = document.getElementById("stream-serv-blocks");
+
+ /*When the user clicks on the button, this function accomplishes the following: the title is selected and passed as an argument to the fetchStreamAvail function,
+ so that the streaming options available (if any) are retrieved. At the end of the process, the modal with clickable links to the pages from the distinct streaming services 
+ will be shown.*/
+
+function getModal(event){
+  console.log("hello");
+  selectedTitle = event.target.innerHTML;
+  console.log(selectedTitle);
+  streamOptsList.innerHTML = "";
+  fetchStreamAvail(selectedTitle);
+}
+
    // When the user clicks on <span> (x), the modal closes.
    span.onclick = function() {
      modal.style.display = "none";
@@ -179,15 +182,15 @@ for(let k=0; k<10; k++){ //Iterating through the first 10 pages in order to find
    
    // When the user clicks anywhere outside of the modal, it will close.
    window.onclick = function(event) {
-     if (event.target !== modal) {
+     if (event.target == modal) {
        modal.style.display = "none";
      }
    } 
    
    
-   function fetchStreamingAvailability(movieTitle, movieData) {
+   function fetchStreamAvail(movieTitle) {
    
-   const XRapidAPIKey = 'aed62a6282msh245eac67e8ef1f5p1a61d3jsn48e680928113';
+   const XRapidAPIKey = '51eb24f287msh9f2cb4653c7af8fp11236fjsne4cdc84cdeab';
    
    const XRapidAPIHost = "streaming-availability.p.rapidapi.com";
    
@@ -206,11 +209,16 @@ for(let k=0; k<10; k++){ //Iterating through the first 10 pages in order to find
    fetch(streamAvailURL, streamAvailOptions)
      .then(function (response) {
        if (!response.ok) { //If the response status is not within the 200s range, then halt the execution of the fetch request
+
+        console.log(response.statusText);
+
          return;
        }
-       var result= response.json();
-       console.log(result);
-       return result;
+
+       console.log(response.json());
+
+       return;
+
      }).then(function (data){
    
        if(!data){
@@ -221,12 +229,13 @@ for(let k=0; k<10; k++){ //Iterating through the first 10 pages in order to find
        console.log(data.result);
        console.log(data.result.length);
    
-         console.log(data.result[0]); //The database offers many similar titles with each search. I decided to go with the first one in the array
-   
-         
-         movieData = [data.result[0].title, data.result[0].year, data.result[0].imdbId, data.result[0].directors];
+       console.log(data.result[0]); //The database offers many similar titles with each search. I decided to go with the first one in the array
+
+       movieStreamOpts = data.result[0].streamingInfo.us;// This array will store the different streaming options available in the US for the given title
    
          console.log(movieData);
+
+         movieData = [data.result[0].title, data.result[0].year, data.result[0].imdbId, data.result[0].directors];
    
          movieInfo = document.getElementById("movie-title");
          movieInfo.innerHTML = movieData[0] + "   "+ movieData[1] + " dir.: "+ movieData[3];
@@ -236,7 +245,7 @@ for(let k=0; k<10; k++){ //Iterating through the first 10 pages in order to find
          movieExtInfo.setAttribute("target", "_blank");
          movieExtInfo.textContent = "Go to the IMDB for more info";
    
-   
+        /*
          console.log(data.result[0].genres); //It can be classified in more than one genre; this property is an array whose elements are objects with 2 properties .id(a number) and .name
          
    
@@ -244,11 +253,19 @@ for(let k=0; k<10; k++){ //Iterating through the first 10 pages in order to find
           
            console.log(data.result[0].genres[j].id);
          }
-   
-         movieStreamOpts = data.result[0].streamingInfo.us;// This array will store the different streaming options available in the US for the given title
-         console.log(movieStreamOpts);
-         console.log(movieStreamOpts.length);
-   
+         */
+        
+         if(!movieStreamOpts){ //If there are no streaming options available, then the modal appears with a message telling so to the user.
+
+           var noOptsEl = document.createElement("li");
+           var noOptsPar = document.createElement("p");
+           noOptsEl.appendChild(noOptsPar);
+           noOptsPar.textContent = "Sorry! There are no streaming options available right now. You can still search for more info about the selected title by clicking on the above link."
+           streamOptsList.appendChild(noOptsEl);
+           modal.style.display = "block";
+
+         };
+        
          for(let k=0; k < movieStreamOpts.length; k++){//Looping through the streaming info.
    
          movieStreamOpt[0] = movieStreamOpts[k].availableSince; //Original date since its been available on the service given as a Unix timestamp;
@@ -256,14 +273,14 @@ for(let k=0; k<10; k++){ //Iterating through the first 10 pages in order to find
          movieStreamOpt[2] = movieStreamOpts[k].quality;//Indicates whether it is in standard, high or ultra-high definition
          movieStreamOpt[3] = movieStreamOpts[k].service;//Name of the streaming service
          movieStreamOpt[4] = movieStreamOpts[k].streamingType;//Indicates whether the service is premium, free, etc.
+
    
-          console.log(movieStreamOpt);
-   
-         document.getElementById("stream-serv-blocks").innerHTML += createStreamServBlock(movieStreamOpt);// Creates a new block within the modal, displaying the info found on movieStreamOpt for each available service option and appends it to the corresponding <div> within the modal.
-   
+         streamOptsList.innerHTML += createStreamServBlock(movieStreamOpt);// Creates a new block within the modal, displaying the info found on movieStreamOpt for each available service option and appends it to the corresponding <div> within the modal.
+
          movieStreamOpt = []; // empties the array prior to the next loop iteration
 
          };
+         modal.style.display ="block";
            
        });
    
@@ -276,14 +293,14 @@ for(let k=0; k<10; k++){ //Iterating through the first 10 pages in order to find
                    <a href="${movieStreamOpt[1]}" target ="_blank" class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
                        <span class="flex-1 ml-3 whitespace-pre"">${String(movieStreamOpt[3]).toUpperCase()}</span>
                        <span class="flex-1 ml-3 whitespace-pre">Availible since: </span>
-                       <span class="flex-1 ml-3 whitespace-pre">${dayjs.unix(movieStreamOpt[0])}</span>"
+                       <span class="flex-1 ml-3 whitespace-pre">${dayjs(dayjs.unix(movieStreamOpt[0])).format("ddd, D MMM, YYYY")}</span>"
                        <span class="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">${movieStreamOpt[2]}</span>
                        <span class="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">${movieStreamOpt[4]}</span>
                      </a>
                </li>`;
      }
     
-/* This are all the genres available, but the classification is really bad
+/* This are all the genres available, but the classification is really bad. Maybe we can add more to the dropdown menu.
 {
   "genres": [
     {
